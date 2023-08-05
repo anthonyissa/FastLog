c15c1c07b727b590ec80cc10aa52a80284641951
Madderstone <anthony.issa@hotmail.fr>
2023-08-05 15:54:56 +0200

    removed metadata

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -2,7 +2,6 @@
 
 import "@/styles/globals.css"
 import React, { Component, useEffect, useState } from "react"
-import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -12,22 +11,6 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 import { AppWrapper } from "./session-context"
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
 
 interface RootLayoutProps {
   children: React.ReactNode
