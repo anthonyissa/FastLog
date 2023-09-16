05153e6d92da6671ae8c61a9cabb1363231440a6
Madderstone <anthony.issa@hotmail.fr>
2023-09-16 19:01:13 +0200

    resized header

--- front-end/components/site-header.tsx
+++ front-end/components/site-header.tsx
@@ -15,7 +15,7 @@ export function SiteHeader() {
 
   return (
     <header className="sticky top-0 z-40 w-full bg-transparent">
-      <div className="container flex h-16 max-w-6xl items-center space-x-4 sm:justify-between sm:space-x-0">
+    <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
