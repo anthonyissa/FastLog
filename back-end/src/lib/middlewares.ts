79147aedc6b9b0f12517f6ae9cd95cc3a6dfe966
Madderstone <anthony.issa@hotmail.fr>
2023-10-24 11:00:15 +0200

    test rate limit

--- back-end/src/lib/middlewares.ts
+++ back-end/src/lib/middlewares.ts
@@ -1,24 +1,23 @@
-import { rateLimit } from 'express-rate-limit';
-import jwt from 'jsonwebtoken';
+import { rateLimit } from "express-rate-limit";
+import jwt from "jsonwebtoken";
 
-export const verifyJwt = (req, res, next)=> {
-    const token = req.headers["x-api-key"];
-    if (!token) {
-        return res.status(401).json({ error: "No JWT provided" });
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.sub;
-        next();
-    }
-    catch (error) {
-        console.error(error);
-        return res.status(401).json({ error: "Invalid JWT" });
-    }
-}
+export const verifyJwt = (req, res, next) => {
+  const token = req.headers["x-api-key"];
+  if (!token) {
+    return res.status(401).json({ error: "No JWT provided" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.sub;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ error: "Invalid JWT" });
+  }
+};
 
 export const rateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: 'Too many requests, please try again later',
+  max: 1, // Limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later",
 });
