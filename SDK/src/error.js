4e02fac7e9225d9e9dd7d0b2ec93a315ee9c60a3
Madderstone <anthony.issa@hotmail.fr>
2023-11-08 09:29:14 +0100

    removed socket

--- SDK/src/error.js
+++ SDK/src/error.js
@@ -1,29 +1,55 @@
-import { originalLogger, sendToFastLog } from "./logs.js";
+import axios from "axios";
+import { id, originalLogger, sendToFastLog, userId } from "./logs.js";
+import { apiUrl } from "./utils.js";
 
-process.on("uncaughtException", (error) => {
-  originalLogger(error);
-  sendToFastLog(
-    "ERROR",
-    JSON.stringify({
-      name: error.name,
-      error: error.message,
-      stack: error.stack,
-      code: error.code,
-      reason: error.reason,
-    })
-  );
-});
+export const activateHealthCheck = () => {
+  setStatus("UP");
 
-process.on("unhandledRejection", (error) => {
-  originalLogger(error);
-  sendToFastLog(
-    "ERROR",
-    JSON.stringify({
-      name: error.name,
-      error: error.message,
-      stack: error.stack,
-      code: error.code,
-      reason: error.reason,
-    })
-  );
-});
+  process.on("uncaughtException", (error) => {
+    originalLogger(error);
+    sendToFastLog(
+      "ERROR",
+      JSON.stringify({
+        name: error.name,
+        error: error.message,
+        stack: error.stack,
+        code: error.code,
+        reason: error.reason,
+      })
+    );
+    setStatus("DOWN");
+    setTimeout(() => {
+      process.exit(1);
+    }, 500);
+  });
+
+  process.on("unhandledRejection", (error) => {
+    originalLogger(error);
+    sendToFastLog(
+      "ERROR",
+      JSON.stringify({
+        name: error.name,
+        error: error.message,
+        stack: error.stack,
+        code: error.code,
+        reason: error.reason,
+      })
+    );
+    setStatus("DOWN");
+    setTimeout(() => {
+      process.exit(1);
+    }, 500);
+  });
+};
+
+export const setStatus = (status) => {
+  axios(apiUrl + "/apps/status", {
+    method: "POST",
+    data: JSON.stringify({ user_id: userId, app_id: id, status }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).catch((err) => {
+    originalLogger("Error sending log to Fastlog", err);
+  });
+};
--- back-end/src/model/errors.ts
+++ back-end/src/model/errors.ts
@@ -1,13 +1,24 @@
 export class MissingRequiredFieldsError extends Error {
-    constructor(message?: string) {
-        super(message || "");
-        this.name = "MissingRequiredFieldsError";
-    }
+  constructor(message?: string) {
+    super(message || "");
+    this.name = "MissingRequiredFieldsError";
+  }
 }
 
 export class InvalidWebhookMethodError extends Error {
-    constructor(message: string = "Invalid webhook method. Valid methods are GET and POST") {
-        super(message);
-        this.name = "InvalidWebhookMethodError";
-    }
+  constructor(
+    message: string = "Invalid webhook method. Valid methods are GET and POST"
+  ) {
+    super(message);
+    this.name = "InvalidWebhookMethodError";
+  }
+}
+
+export class InvalidStatusError extends Error {
+  constructor(
+    message: string = "Invalid status. Valid statuses are UP and DOWN"
+  ) {
+    super(message);
+    this.name = "InvalidStatusError";
+  }
 }
